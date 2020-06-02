b1365a43790ffa7e8160a6361bfc28f9c41447b2
nfoth <n@foth.com>
2020-06-02 15:33:02 +0200

    undo http changes

--- src/app/_services/hero.service.ts
+++ src/app/_services/hero.service.ts
@@ -3,7 +3,6 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heros';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -20,12 +19,5 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-  this.messageService.add(`HeroService: ${message}`);
-  }
-
-  constructor(
-    private http: HttpClient,
-    private messageService: MessageService) { }
+  constructor(private messageService: MessageService) { }
 }
